c32414ef2897b6a50a2c979e2ab61ea1bb7e7ba1
sardorpro004 <sardorsadullayev575@gmail.com>
2026-06-30 02:29:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 import asyncio
-
+from database import create_db
 from config import BOT_TOKEN
 from handlers.start import router as start_router
 bot = Bot(
@@ -14,7 +14,7 @@ dp = Dispatcher()
 dp.include_router(start_router)
 async def main():
     print("Bot ishga tushdi...")
-
+await create_db()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
