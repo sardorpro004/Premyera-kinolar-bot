f88ea6b234775647423e3f422491090f833f268b
sardorpro004 <sardorsadullayev575@gmail.com>
2026-06-30 19:15:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-import asyncio
-from database import create_db
+
 from config import BOT_TOKEN
+from database import create_db
+
 from handlers.start import router as start_router
 from handlers.admin import router as admin_router
 from handlers.movie import router as movie_router
+
+
+bot = Bot(
+    token=BOT_TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
+
 dp = Dispatcher()
 
 dp.include_router(start_router)
 dp.include_router(admin_router)
 dp.include_router(movie_router)
+
+
 async def main():
-    print("Bot ishga tushdi...")
-await create_db()
+    await create_db()
+    print("✅ Bot ishga tushdi...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
