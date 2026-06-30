c452fe2610f19e4d65a3c6422a5c48f34cdb8d18
sardorpro004 <sardorsadullayev575@gmail.com>
2026-06-30 12:35:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
 BOT_TOKEN = "8356918232:AAHR4JCa6QW70cKcE3P2kVxqZIoS6BS3ebs"
+
 ADMIN_ID = 8037410173
+
+CHANNELS = [
+    "@pro_kinolar_n1",
+    "@clash_akk_savdo",
+    "@clash_of_clans_akaunt_savdo_chat"
+]
